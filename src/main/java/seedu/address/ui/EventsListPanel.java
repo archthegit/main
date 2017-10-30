306791135fb77bb2e66e797cc2e61e40bd5a1314
Archana Pradeep <archieprad@gmail.com>
2017-10-30 22:33:37 +0800

    Resolving issues with UI

--- src/main/java/seedu/address/ui/EventsListPanel.java
+++ src/main/java/seedu/address/ui/EventsListPanel.java
@@ -53,7 +53,7 @@ public class EventsListPanel extends UiPart<Region> {
     }
 
     /**
-    * Scrolls to the {@code PersonCard} at the {@code index} and selects it.
+    * Scrolls to the {@code EventsCard} at the {@code index} and selects it.
     */
     private void scrollTo(int index) {
         Platform.runLater(() -> {
