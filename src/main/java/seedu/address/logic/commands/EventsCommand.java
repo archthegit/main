e960f0c8a1b490bbea2d9cf7af828d00b83a6320
Archana Pradeep <archieprad@gmail.com>
2017-10-30 19:46:09 +0800

    modified events command

--- src/main/java/seedu/address/logic/commands/EventsCommand.java
+++ src/main/java/seedu/address/logic/commands/EventsCommand.java
@@ -8,7 +8,7 @@ public class EventsCommand extends Command {
     public static final String COMMAND_WORD = "events";
     public static final String COMMAND_ALIAS = "ev";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists all events for the next month\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists all events for the next 30 days\n"
             + "Example: " + COMMAND_WORD;
 
     private final EventPredicate predicate = new EventPredicate();
